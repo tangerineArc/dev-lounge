66aa7601b1a5bb6d243e885a9f78b97f78d79a04
Swagatam <swagatamheroz@outlook.com>
2025-03-15 18:20:40 +0530

    feat: set up primary controllers

--- controllers/index-controllers.js
+++ controllers/index-controllers.js
@@ -5,72 +5,123 @@ import passport from "passport";
 
 import * as db from "../db/queries.js";
 
-import validateUser from "../validators/user-validator.js";
+import formatDate from "../utils/format-date.js";
+
+import validateMember from "../validators/member-validator.js";
 
 const renderHomePage = (req, res) => {
-  res.render("index.ejs");
+  res.render("home-page.ejs");
 };
 
-const renderRegisterPage = (req, res) => {
-  res.render("register.ejs");
-};
+const renderSignInPage = (req, res) => {
+  const errorMessages = req.session.messages;
+  req.session.messages = [];
 
-const renderLoginPage = (req, res) => {
-  res.render("login.ejs");
-};
+  const successMessages = [];
+  if (req.query["account-created"] === "1") {
+    successMessages.push("Account created successfully.");
+    successMessages.push("Sign in for more fun.");
+  }
 
-const renderConfidentialPage = (req, res) => {
-  res.render("confidential.ejs", { isAuthenticated: req.isAuthenticated() });
-};
-
-const renderLoginSuccessPage = (req, res) => {
-  res.render("login-success.ejs");
-};
-
-const logoutUser = (req, res, next) => {
-  req.logout((err) => {
-    if (err) return next(err);
-    res.redirect("/protected-route");
+  res.render("form-page.ejs", {
+    title: "DevLounge | Sign In",
+    page: "sign-in",
+    actionRoute: "/sign-in",
+    errorMessages,
+    successMessages,
   });
 };
 
-const registerNewUser = [
-  validateUser,
+const renderSignUpPage = (req, res) => {
+  res.render("form-page.ejs", {
+    title: "DevLounge | Sign Up",
+    page: "sign-up",
+    actionRoute: "/sign-up",
+  });
+};
+
+const renderProfilePage = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const member = await db.selectMemberById(id);
+  const posts = await db.selectAllPostsByUserId(id);
+
+  let samePerson = false;
+  if (Number(id) === req.user?.id) {
+    samePerson = true;
+  }
+
+  res.render("profile-page.ejs", { member, posts, samePerson, formatDate });
+});
+
+const renderBecomeKnightPage = (req, res) => {
+  res.render("riddle-page.ejs");
+};
+
+const signInMember = passport.authenticate("local", {
+  failureRedirect: "/sign-in",
+  successRedirect: "/posts",
+  failureMessage: true,
+});
+
+const signOutMember = (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+};
+
+const signUpMember = [
+  validateMember,
   asyncHandler(async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new Error(
-        errors
-          .array()
-          .map((err) => err.msg)
-          .join(" :: ")
-      );
+      const { username, firstName, lastName } = req.body;
+
+      return res.render("form-page.ejs", {
+        title: "DevLounge | Sign Up",
+        page: "sign-up",
+        actionRoute: "/sign-up",
+        errorMessages: errors.array().map((err) => err.msg),
+        defaults: { username, firstName, lastName },
+      });
     }
 
-    const { username, password } = req.body;
-    const passwordHash = await bcrypt.hash(
-      password,
-      Number(process.env.SALT_LENGTH)
-    );
+    const { username, password, firstName, lastName } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
 
-    await db.saveUser({ username, passwordHash });
+    await db.insertNewMember({
+      username,
+      passwordHash,
+      firstName,
+      lastName,
+    });
 
-    res.redirect("/");
+    res.redirect("/sign-in?account-created=1");
   }),
 ];
 
-const loginUser = passport.authenticate("local", {
-  failureRedirect: "/login-failure",
-  successRedirect: "/login-success",
+const makeMemberKnight = asyncHandler(async (req, res) => {
+  const { answer } = req.body;
+
+  if (answer.trim() === process.env.RIDDLE_ANSWER) {
+    await db.updateMemberStatusToKnight(req.user.id);
+
+    return res.redirect("/posts");
+  }
+
+  res.render("riddle-page.ejs", {
+    errorMessage: "Your answer is not quite right",
+  });
 });
 
 export {
-  loginUser,
-  logoutUser,
-  registerNewUser,
-  renderConfidentialPage,
+  makeMemberKnight,
+  renderBecomeKnightPage,
   renderHomePage,
-  renderLoginPage,
-  renderLoginSuccessPage,
-  renderRegisterPage,
+  renderProfilePage,
+  renderSignInPage,
+  renderSignUpPage,
+  signInMember,
+  signOutMember,
+  signUpMember,
 };
